bb7e9713843e0687f6170fe7968e4a4337fa5036
KaterinaSSlavova <k.slavova@student.fontys.nl>
2025-06-10 19:42:44 +0200

    creating registration acceptance test

---
+++ Acceptance Testing/Features/UserRegistration.feature
@@ -0,0 +1,8 @@
+﻿Feature: User Registration
+
+Scenario: Successful user registration
+Given I am on the registration page
+When I enter a valid registration details
+And I click the "Register" button
+Then my account should be created
+And I should be redirected to Log in page
---
+++ Acceptance Testing/Steps/UserRegistrationSteps.cs
@@ -0,0 +1,68 @@
+﻿using Bookly.Bookly.Controllers;
+using Bookly.ViewModels;
+using Interfaces;
+using Moq;
+using TechTalk.SpecFlow;
+using AutoMapper;
+using Business_logic.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Acceptance_Testing.Steps
+{
+    [Binding]
+    public class UserRegistrationSteps
+    {
+        private AccountRegister _registration;
+        private Mock<IUserServices> _userService;
+        private Mock<IShelfServices> _shelfServices;
+        private Mock<IMapper> _mapper;
+        private UserController _controller;
+        private UserDTO user;
+        private IActionResult _actionResult;
+
+        [Given(@"I am on the registration page")]
+        public void GivenIAmOnTheRegistrationPage()
+        {
+            _registration = new AccountRegister();
+            _userService = new Mock<IUserServices>();
+            _shelfServices = new Mock<IShelfServices>();
+            _mapper = new Mock<IMapper>();
+            _controller = new UserController(_userService.Object, _shelfServices.Object, _mapper.Object);
+        }
+
+        [When(@"I enter a valid registration details")]
+        public void WhenIEnterValidRegistrationDetails()
+        {
+            _registration.Username = "New User";
+            _registration.Email = "user@mail.com";
+            _registration.Password = "password";
+            _registration.ConfirmPassword = "password";
+        }
+
+        [When(@"I click the ""Register"" button")]
+        public void WhenIClickTheRegisterButton()
+        {
+            user = new UserDTO(_registration.Username, _registration.Email, _registration.Password);
+            _mapper.Setup(m => m.Map<UserDTO>(_registration))
+                .Returns(user);
+
+            _userService.Setup(s => s.Register(user)).Verifiable();
+
+            _actionResult = _controller.Register(_registration);
+        }
+
+        [Then(@"my account should be created")]
+        public void ThenMyAccountShouldBeCreated()
+        {
+            _userService.Verify(s => s.Register(user), Times.Once);
+        }
+
+        [Then(@"I should be redirected to Log in page")]
+        public void IShouldBeRedirectedToLogInPage()
+        {
+            var redirectResult = _actionResult as RedirectToActionResult;
+            Assert.IsNotNull(redirectResult);
+            Assert.AreEqual(redirectResult.ActionName, "LogIn");
+        }
+    }
+}
